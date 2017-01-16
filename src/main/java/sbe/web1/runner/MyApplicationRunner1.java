d2842b65f4a649e612f6d77f88cb57639be5b332
sakamoto <sakamoto@securesky-tech.com>
2017-01-16 15:49:19 +0900

    解説コメント追記 (sbe.web1.runner)

--- src/main/java/sbe/web1/runner/MyApplicationRunner1.java
+++ src/main/java/sbe/web1/runner/MyApplicationRunner1.java
@@ -3,6 +3,10 @@ package sbe.web1.runner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+/**
+ * {@link org.springframework.boot.ApplicationRunner} を複数作った場合に、 Order
+ * アノテーションで順序を制御するデモ
+ */
 @Component
 @Order(1)
 public class MyApplicationRunner1 extends MyApplicationRunner {
--- src/main/java/sbe/web1/runner/MyCommandLineRunner2.java
+++ src/main/java/sbe/web1/runner/MyCommandLineRunner2.java
@@ -3,6 +3,10 @@ package sbe.web1.runner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+/**
+ * {@link org.springframework.boot.CommandLineRunner} を複数作った場合に、 Order
+ * アノテーションで順序を制御するデモ
+ */
 @Component
 @Order(2)
 public class MyCommandLineRunner2 extends MyCommandLineRunner {
